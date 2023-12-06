af6e67f5ece8d9a8f862ed9ef197ebcf0bc513d9
OrcaSuit <orcasuit@gmail.com>
2023-12-06 11:41:17 +0000

    풀 받기전 커밋

--- userprog/syscall.c
+++ userprog/syscall.c
@@ -1,17 +1,19 @@
 #include "userprog/syscall.h"
+
 #include <stdio.h>
 #include <syscall-nr.h>
-#include "threads/interrupt.h"
-#include "threads/thread.h"
-#include "threads/loader.h"
-#include "userprog/gdt.h"
-#include "threads/flags.h"
+
+#include "filesys/filesys.h"
 #include "intrinsic.h"
+#include "threads/flags.h"
+#include "threads/interrupt.h"
+#include "threads/loader.h"
+#include "threads/thread.h"
+#include "userprog/gdt.h"
 
-void syscall_entry (void);
-void syscall_handler (struct intr_frame *);
+void syscall_entry(void);
+void syscall_handler(struct intr_frame *);
 struct file *get_file_from_fd(int fd);
-
 
 /* 시스템 콜.
  *
@@ -19,43 +21,47 @@ struct file *get_file_from_fd(int fd);
  * (예: 리눅스에서 int 0x80). 그러나 x86-64에서는 제조업체가
  * 시스템 콜을 요청하기 위한 효율적인 경로인 `syscall` 명령어를 제공합니다.
  *
- * syscall 명령어는 모델 특정 레지스터(Model Specific Register, MSR)에서 값을 읽어 작동합니다.
- * 자세한 내용은 매뉴얼을 참조하세요. */
+ * syscall 명령어는 모델 특정 레지스터(Model Specific Register, MSR)에서 값을
+ * 읽어 작동합니다. 자세한 내용은 매뉴얼을 참조하세요. */
 
 #define MSR_STAR 0xc0000081         /* 세그먼트 셀렉터 MSR */
 #define MSR_LSTAR 0xc0000082        /* 롱 모드 SYSCALL 대상 */
 #define MSR_SYSCALL_MASK 0xc0000084 /* eflags에 대한 마스크 */
 
-void
-syscall_init (void) {
-	write_msr(MSR_STAR, ((uint64_t)SEL_UCSEG - 0x10) << 48  |
-			((uint64_t)SEL_KCSEG) << 32);
-	write_msr(MSR_LSTAR, (uint64_t) syscall_entry);
+void syscall_init(void) {
+    write_msr(MSR_STAR,
+              ((uint64_t)SEL_UCSEG - 0x10) << 48 | ((uint64_t)SEL_KCSEG) << 32);
+    write_msr(MSR_LSTAR, (uint64_t)syscall_entry);
 
-	/* 인터럽트 서비스 루틴은 syscall_entry가 사용자 영역 스택을 커널
-	 * 모드 스택으로 전환할 때까지 어떠한 인터럽트도 처리하지 않아야 합니다.
-	 * 따라서, FLAG_FL을 마스킹했습니다. */
-	write_msr(MSR_SYSCALL_MASK,
-			FLAG_IF | FLAG_TF | FLAG_DF | FLAG_IOPL | FLAG_AC | FLAG_NT);
+    /* 인터럽트 서비스 루틴은 syscall_entry가 사용자 영역 스택을 커널
+     * 모드 스택으로 전환할 때까지 어떠한 인터럽트도 처리하지 않아야 합니다.
+     * 따라서, FLAG_FL을 마스킹했습니다. */
+    write_msr(MSR_SYSCALL_MASK,
+              FLAG_IF | FLAG_TF | FLAG_DF | FLAG_IOPL | FLAG_AC | FLAG_NT);
 }
 
-/* 주요 시스템 콜 인터페이스 */
-// void
-// syscall_handler (struct intr_frame *f UNUSED) {
-// 	// TODO: 여기에 구현이 들어갑니다.
-// 	printf ("system call!\n");
-// 	thread_exit ();
-// }
+void check_address(const void *addr) {
+    // 주소가 NULL이거나 유효한 사용자 주소가 아닌 경우
+    if (addr == NULL || !is_user_vaddr(addr)) {
+        // 에러 메시지 출력 및 현재 스레드 종료
+        printf("Error: Invalid memory access attempt.\n");
+        thread_exit();
+    }
 
-void check_address(void *addr) {
-	if (!is_user_vaddr(addr)) {
-		
-	}
+    // 여기에 추가적인 메모리 검증 로직을 추가할 수 있습니다.
+    // 예: 페이지 테이블에서 해당 주소가 가리키는 페이지가 존재하는지 확인
 }
+
+bool create(const char *file, unsigned initial_size) {
+    if (file == NULL) exit(-1);
+    bool result = (filesys_create(file, initial_size));
+    printf("파일 생성 완료");
+    return result;
+}
+
 void syscall_handler(struct intr_frame *f) {
     // 시스템 콜 번호를 RAX 레지스터로부터 읽어옵니다.
-	//check_address(&f->rsp);
-
+    // check_address(&f->rsp);
 
     int syscall_number = f->R.rax;
 
@@ -63,37 +69,39 @@ void syscall_handler(struct intr_frame *f) {
     int syscall_result = -1;
 
     // 시스템 콜 번호에 따라 적절한 처리 수행
-    switch (syscall_number) {
+    switch (5) {
         case SYS_HALT:
             // ... halt 처리 ...
-            power_off();
             break;
         case SYS_EXIT:
             // ... exit 처리 ...
-            //exit(f->R.rdi);  // 예를 들어, exit 시스템 콜의 인자는 RDI 레지스터에 저장됩니다.
+            // exit(f->R.rdi);  // 예를 들어, exit 시스템 콜의 인자는 RDI
+            // 레지스터에 저장됩니다.
             break;
-		case SYS_FORK:
-			break;
-		case SYS_EXEC:
-			break;
-		case SYS_WAIT:
-			break;
-		case SYS_CREATE:
-			break;
-		case SYS_REMOVE:
-			break;
-		case SYS_OPEN:
-			break;
-		case SYS_FILESIZE:
-			break;
-		case SYS_READ:
-			break;
-		case SYS_WRITE:
-			break;
-		case SYS_TELL:
-			break;
-		case SYS_CLOSE:
-			break;
+        case SYS_FORK:
+            break;
+        case SYS_EXEC:
+            break;
+        case SYS_WAIT:
+            break;
+        case SYS_CREATE:
+            check_address(f->R.rdi);
+            f->R.rax = create(f->R.rdi, f->R.rsi);
+            break;
+        case SYS_REMOVE:
+            break;
+        case SYS_OPEN:
+            break;
+        case SYS_FILESIZE:
+            break;
+        case SYS_READ:
+            break;
+        case SYS_WRITE:
+            break;
+        case SYS_TELL:
+            break;
+        case SYS_CLOSE:
+            break;
         default:
             printf("Unknown system call number: %d\n", syscall_number);
             break;
