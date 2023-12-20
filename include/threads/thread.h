8531ed459be380cd6bc182add073bbcbc02be73a
kim5606 <104742263+kim5606@users.noreply.github.com>
2023-12-20 20:14:39 +0900

    1220
    1220

--- include/threads/thread.h
+++ include/threads/thread.h
@@ -112,6 +112,8 @@ struct thread {
 #ifdef VM
     /* Table for whole virtual memory owned by thread. */
     struct supplemental_page_table spt;
+    // project 3-2 stack growth
+    uintptr_t user_rsp;
 #endif
 
     /* Owned by thread.c. */
